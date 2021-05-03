e33a1a0fb943442c24ebb702502c46b77eedb9bb
Teddy Wing <git@teddywing.com>
2021-05-03 23:17:12 +0200

    main: Remove debug print lines

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,12 @@ fn main() {
     let mut doc = Document::load("./f1040.pdf").unwrap();
 
     for (_, mut obj) in &mut doc.objects {
-        // println!("{:?}", obj);
-
         match &mut obj {
             Object::Dictionary(ref mut d) => {
                 for (k, v) in d.iter_mut() {
                     let key = std::str::from_utf8(k).unwrap();
 
                     if key == "DA" {
-                        // println!("{:?}", d);
-
                         let properties = v.as_str_mut().unwrap();
 
                         let new_properties = std::str::from_utf8(properties)
@@ -22,9 +18,6 @@ fn main() {
                             .replace("HelveticaLTStd-Bold", "CourierNewPSMT");
 
                         *properties = new_properties.into_bytes();
-
-                        dbg!(std::str::from_utf8(properties).unwrap());
-                        // dbg!(properties);
                     }
                 }
             },
